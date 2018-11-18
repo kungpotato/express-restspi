911cb3d8715ffa313cec1bbd430711e66ed9c9e0
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-11-18 21:20:28 +0700

    http get request

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req,res){
+        var responseJson = {hello: "This is my api"}
+        res.json(responseJson)
+        // var query = {};
+
+        // if(req.query.genre)
+        // {
+        //     query.genre = req.query.genre;
+        // }
+        // Book.find(query, function(err,books){
+        //     if(err)
+        //         res.status(500).send(err);
+        //     else
+        //         res.json(books);
+        // });
+    });
+
+app.use('/api', bookRouter);
+
+
 app.get('/', function(req, res){
     res.send('welcome to my API!');
 });
