03e37f4d5161274f1a16a61c3039064fffea86c0
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-11-19 20:32:29 +0700

    making router

--- app.js
+++ app.js
@@ -13,49 +13,10 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var bookRouter = express.Router();
+bookRouter = require('./Routes/bookRoutes')(Book); // inject model เข้าไป
 
-bookRouter.route('/Books')
-    .post((req, res)=>{
-        var book = new Book(req.body) // ต้องลง body-parser ก่อน
-        //console.log(book)
-        book.save()
-        res.status(201).send(book) // 201 is created ส่ง id กลับไปหาคน call
-    })
-    .get((req,res)=>{
-        // var responseJson = {hello: "This is my api"}
-        // res.json(responseJson)
-
-        var query = {};
-
-        if(req.query.genre)
-        // ถ้ามีค่าให้แสดง
-        {
-            query.genre = req.query.genre;
-        }
-
-        Book.find(query, function(err,books){
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(books);
-        });
-    });
-
-bookRouter.route('/Books/:bookId')
-    .get(function(req,res){
-
-
-        Book.findById(req.params.bookId, function(err,book){
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(book);
-        });
-    });
-
-app.use('/api', bookRouter);
-
+app.use('/api/books', bookRouter);
+// app.use('/api/author', authorRouter);
 
 app.get('/', function(req, res){
     res.send('welcome to my API!');
