c9e14336438766a6066722a2b69fc2c08b21f340
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2019-03-09 20:26:42 +0700

    update3

--- models/MasterMaterial.js
+++ models/MasterMaterial.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var MasterMaterial = new Schema({
-    material: {type: String}
+    material: {type: String},
+    isChecked: {type: Boolean, default: false }
 });
 
 module.exports= mongoose.model('MasterMaterial', MasterMaterial);
