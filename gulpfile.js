bd8141313ac1fc4680ee282577b173b3ad5fb708
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2019-03-10 15:38:10 +0700

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,24 @@
-var gulp = require('gulp'),
-    nodemon = require('gulp-nodemon'),
-    gulpMocha = require('gulp-mocha'),
-    env = require('gulp-env'),
-    supertest = require('supertest')
+var gulp = require('gulp')
+var nodemon = require('gulp-nodemon')
+// var env = require('gulp-env')
+// var gulpMocha = require('gulp-mocha')
 
-
-gulp.task('default', function(){
-    nodemon({
-        script: 'app.js',
-        ext: 'js',
-        env: {
-            PORT:8000
-        },
-        ignore: ['./node_modules/**']
+gulp.task('default', () => {
+  nodemon({
+    script: 'app.js',
+    ext: 'js',
+    env: {
+      PORT: 8000
+    },
+    ignore: ['./node_modules/**']
+  })
+    .on('restart', () => {
+      console.log('Restarting')
     })
-    .on('restart', function(){
-        console.log('Restarting');
-    });
-});
+})
 
-gulp.task('test', function(){
-    env({vars: {ENV:'Test'}});
-    gulp.src('tests/*.js', {read: false})
-        .pipe(gulpMocha({reporter: 'nyan'}))
-});
+// gulp.task('test', function(){
+//     env({vars: {ENV:'Test'}});
+//     gulp.src('tests/*.js', {read: false})
+//         .pipe(gulpMocha({reporter: 'nyan'}))
+// });
